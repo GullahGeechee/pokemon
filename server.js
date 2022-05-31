6a77b00ac9a5ad335a0585c1eb8481dfbb00d042
Raven <user@users-MacBook-Air.local>
2022-05-31 12:00:16 -0400

    Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { render } = require('ejs');
 const express = require('express')
 const pokemon = require("./models/pokemon")
 const app = express()
@@ -12,6 +13,10 @@ app.set('view engine', 'ejs');
 app.get("/", (req, res) => {
     //console.log(req);
     res.send("Welcome ot the Pokemon App!");
+})
+//creating url not folder 
+app.get("/pokemon", (req, res) => {
+    res.render("index", {data: pokemon})
 })
 
 
