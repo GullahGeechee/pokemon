cfa25689907a8f4e4b9385a8b5b68c3b24ae376b
Raven <user@users-MacBook-Air.local>
2022-05-31 11:40:40 -0400

    my server is set up and running

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const pokemon = require("./models/pokemon")
+const app = express()
+
+const port = 3000
+
+
+app.set('view engine', 'ejs'); 
+//app.set('views', './views' );
+
+
+app.get("/", (req, res) => {
+    //console.log(req);
+    res.send("Welcome ot the Pokemon App!");
+})
+
+
+
+app.listen(port, () => {
+    console.log("RUN RUN RUN");
+})
+
